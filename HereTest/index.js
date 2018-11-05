e3ed479fb8bcd5d113843553b78d68397543294d
SaakethC <saakethrchenna@gmail.com>
2018-11-04 22:16:10 -0600

    got rid of info bubbles ad moved to log only, but the log of the contents doenn't show html, migh have to find diffent way to do this

--- HereTest/index.js
+++ HereTest/index.js
@@ -1,34 +1,10 @@
 //define locations
-  var LocLBNagar = new H.map.Marker({lat: 17.355537, lng: 78.547017});
-  var LocKothapet = new H.map.Marker({lat: 17.366300,lng: 78.542448});
-  var LocLingampally = new H.map.Marker({lat: 17.490932,lng: 78.314697});
-  var LocNalgonda = new H.map.Marker({lat: 17.065101,lng: 79.265834});
-  var LocMehdipatnam = new H.map.Marker({lat: 17.394991,lng: 78.441153});
 //function to add markers to a group, used latter in the info bubbles
-function addMarkerToGroup(group, marker, html) {
+/*function addMarkerToGroup(group, marker, html) {
   marker.setData(html);
   group.addObject(marker);
 }
-//create info bubble from group you created above
-function addInfoBubble(map) {
-  var group = new H.map.Group();
-
-  map.addObject(group);
-
-  // add 'tap' event listener, that opens info bubble, to the group
-  group.addEventListener('tap', function (evt) {
-    // event target is the marker itself, group is a parent event target
-    // for all objects that it contains
-    var bubble =  new H.ui.InfoBubble(evt.target.getPosition(), {
-      // read custom data
-      content: evt.target.getData()
-    });
-    // show info bubble
-    ui.addBubble(bubble);
-  }, false);
-
-    //add the text, html in string form, to the objects you created earlier
-  addMarkerToGroup(group, LocLBNagar,
+    addMarkerToGroup(group, LocLBNagar,
     '<div><a href=\'http://www.mcfc.co.uk\' >LB Nagar</a>' +
     '</div><div >City of Manchester Stadium<br>Capacity: 48,000</div>');
   addMarkerToGroup(group, LocKothapet,
@@ -44,16 +20,27 @@ function addInfoBubble(map) {
     '<div ><a href=\'http://www.liverpoolfc.tv\' >Mehdipatnam</a>' +
     '</div><div >Anfield<br>Capacity: 45,362</div>');
 
-}
-// setting the output values for each element, can replace with html latter
-function testDelegation(map){
-LocLBNagar.setData('LB Nagar');
+*/
+  var LocLBNagar = new H.map.Marker({lat: 17.355537, lng: 78.547017});
+  var LocKothapet = new H.map.Marker({lat: 17.366300,lng: 78.542448});
+  var LocLingampally = new H.map.Marker({lat: 17.490932,lng: 78.314697});
+  var LocNalgonda = new H.map.Marker({lat: 17.065101,lng: 79.265834});
+  var LocMehdipatnam = new H.map.Marker({lat: 17.394991,lng: 78.441153});
+    
+  LocLBNagar.setData('<h1>LB Nagar'+'ajfsdnk</h1>');
 LocKothapet.setData('Kothapet');
 LocLingampally.setData('Lingampally');
 LocNalgonda.setData('Nalgonda');
 LocMehdipatnam.setData('Mehidipatnam!');
 
- group.addEventListener('tap', function (evt) {
+function testDelegation(map) {    
+    var container = new H.map.Group({
+    objects: [LocLBNagar, LocKothapet, LocLingampally, LocNalgonda, LocMehdipatnam]
+  });
+    /*add the text, html in string form, to the objects you created earlier
+*/
+// setting the output values for each element, can replace with html latter
+   container.addEventListener('tap', function (evt) {
     // Now lets log the event
     customLog(evt.target.getData());
   });
@@ -62,7 +49,7 @@ LocMehdipatnam.setData('Mehidipatnam!');
   // Make objects visible by adding them to the map
   map.addObject(container);
 }
-
+    
 /**
  * Boilerplate map initialization code starts below:
  */
@@ -95,8 +82,6 @@ var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 // Create the default UI components
 var ui = H.ui.UI.createDefault(map, defaultLayers);
 
-
-// Step 5: create custom logging facilities
 var logContainer = document.createElement('ul');
 logContainer.className ='log';
 logContainer.innerHTML = '<li class="log-entry">Try clicking on elements</li>';
@@ -110,10 +95,5 @@ function customLog(log) {
   logContainer.insertBefore(entry, logContainer.firstChild);
 }
 
-
-
-//add markers and info bubbles to the map 
-addInfoBubble(map);
-
+// Step 6: main logic goes here
 testDelegation(map, customLog);
-
